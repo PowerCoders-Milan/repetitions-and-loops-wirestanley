0f933b2470be634fb55893a5395b5a04e863a79c
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-03-01 12:14:34 +0100

    exercise2

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,3 +5,10 @@
   // check if the current is even or odd
 
   // report the result to the screen
+  var i;
+  for (i = 0; i <= 20; i++) {
+    if(i%2 ==0)
+    console.log(i + " is even ")
+    else
+    console.log(i + " is odd ")
+  }
