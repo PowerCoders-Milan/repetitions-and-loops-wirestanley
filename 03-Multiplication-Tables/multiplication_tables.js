7f485ba5e143ce1884b798594051106743d758ba
stanleyuhunamure <stanleyuhunamure@gmail.com>
2021-03-01 12:39:51 +0100

    exercise3

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,11 @@
 // write your code below
+//var numbers=9;
+//for (var i = 0; i <= 10; i++) {
+    //var result =numbers*i;
+    //console.log(i);
+ //}
+ var multi = 9;
+for (var i = 0; i <= 10; i++) {
+    var result = multi * i;
+    console.log(multi + ' * ' + i + ' = ' + result);
+}
